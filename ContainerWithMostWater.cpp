e8895c9e2437ece6c7795902b69f88dc27bd11dd
amarjit110 <91938133+amarjit110@users.noreply.github.com>
2021-10-05 12:13:58 +0530

    Update ContainerWithMostWater.cpp

--- ContainerWithMostWater.cpp
+++ ContainerWithMostWater.cpp
@@ -1,6 +1,6 @@
 /*Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
 n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). 
-Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.*/
+Find two lines, which, together with the x-axis forms a container, such that the container contain the most water.*/
 int maxArea(vector<int>& hei) {
     int n=hei.size(),i=0,j=n-1,area=0;
     while(i<j){
